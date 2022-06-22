de90f8961ae208451d7428a68234d52bb510db6b
luciano.nunes <sciasxp@gmail.com>
2022-06-22 10:12:31 -0300

    Remove commentary

--- app.js
+++ app.js
@@ -9,10 +9,5 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use("/", scraperRouter);
 
-// MANGAKAKALOT AND MANGAFOX(can be redone) dont work
-// DEMO DEPLOYED AT: https://manga-reader-express.herokuapp.com/
-// buildpack https://github.com/jontewks/puppeteer-heroku-buildpack.git
-//node app.js > app.log 2>&1
-
 app.listen(process.env.PORT || 4000);
 module.exports = app;
